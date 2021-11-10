0b93376a19612256ae80e02b065df03024cf4438
Yonathan Amare <yamare1@lion.lmu.edu>
2021-11-10 01:23:48 -0800

    Adding files

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,54 @@
-import { useEffect, useState } from "react";
-import Nav from "./Nav";
-import Article from "./Article";
-import ArticleEntry from "./ArticleEntry";
-import { fetchArticles, createArticle } from "../services/articleService";
-import "./App.css";
+import { useEffect, useState } from "react"
+import Nav from "./Nav"
+import Article from "./Article"
+import ArticleEntry from "./ArticleEntry"
+import { SignIn, SignOut, useAuthentication } from "../services/authService"
+import { fetchArticles, createArticle } from "../services/articleService"
+import "./App.css"
 
 export default function App() {
-  const [articles, setArticles] = useState([]);
-  const [article, setArticle] = useState(null);
-  const [writing, setWriting] = useState(null);
+  const [articles, setArticles] = useState([])
+  const [article, setArticle] = useState(null)
+  const [writing, setWriting] = useState(false)
+  const user = useAuthentication()
 
-  // This is a trivial app, so just fetch all the articles once, when
-  // the app is loaded. A real app would do pagination. Note that
+  // This is a trivial app, so just fetch all the articles only when
+  // a user logs in. A real app would do pagination. Note that
   // "fetchArticles" is what gets the articles from the service and
   // then "setArticles" writes them into the React state.
   useEffect(() => {
-    fetchArticles().then(setArticles);
-  }, []);
+    if (user) {
+      fetchArticles().then(setArticles)
+    }
+  }, [user])
 
   // Update the "database" *then* update the internal React state. These
   // two steps are definitely necessary.
   function addArticle({ title, body }) {
     createArticle({ title, body }).then((article) => {
-      setArticle(article);
-      setArticles([article, ...articles]);
-      setWriting(false);
-    });
+      setArticle(article)
+      setArticles([article, ...articles])
+      setWriting(false)
+    })
   }
 
   return (
     <div className="App">
       <header>
-        Blog <button onClick={() => setWriting(true)}>New Article</button>
+        Blog
+        {user && <button onClick={() => setWriting(true)}>New Article</button>}
+        {!user ? <SignIn /> : <SignOut />}
       </header>
-      <Nav articles={articles} setArticle={setArticle} />
-      {writing ? (
+
+      {!user ? "" : <Nav articles={articles} setArticle={setArticle} />}
+
+      {!user ? (
+        ""
+      ) : writing ? (
         <ArticleEntry addArticle={addArticle} />
       ) : (
         <Article article={article} />
       )}
     </div>
-  );
+  )
 }
