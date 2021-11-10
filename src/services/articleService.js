1f9dd03ade1c861f9e147d664158bcbf545394b8
Yonathan Amare <yamare1@lion.lmu.edu>
2021-11-10 01:24:24 -0800

    Adding files

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -2,41 +2,23 @@
 // No other part of the app knows how the data is stored. If anyone wants
 // to read or write data, they have to go through this service.
 
-// For this starter code, we use mock data, that is, data in memory that
-// is pretty arbitrary. The service functions to create and fetch articles
-// will just work on this fake data in memory.
-
-// This intent is for you to fork this starter code and then replace the
-// bodies of the service functions with code that fetches from a *real*
-// database like Firebase Firestore.
-
-const articles = {
-  sdkjfhshfskdlshjf: {
-    date: new Date(2021, 2, 16),
-    title: "There’s a fair tomorrow",
-    body: [
-      "Is a mháithrín an ligfidh tú chun aonaigh mé",
-      "Is a mhuirnín óg ná healaí é",
-      "Beidh aonach amárach in gContae an Chláir",
-      "Cén mhaith domh é ní bheidh mé ann`",
-    ].join("\n"),
-  },
-  asjkdhalfkjsdjfhsd: {
-    date: new Date(2021, 9, 24),
-    title: "Hello Everyone",
-    body: "It is a good day to learn React and Firebase\n".repeat(10),
-  },
-};
+import { db } from "../firebaseConfig"
+import { collection, query, getDocs, addDoc, orderBy, limit, Timestamp } from "firebase/firestore"
 
 export async function createArticle({ title, body }) {
-  // As this is just fake data for messing around, we'll throw in a quick
-  // and unreliable database id. In a real app, the id should be generated
-  // by the database itself (or you can use UUIDs).
-  return { id: Math.random(), title, body, date: new Date() };
+  const data = { title, body, date: Timestamp.now() }
+  const docRef = await addDoc(collection(db, "articles"), data)
+  return { id: docRef.id, ...data }
 }
 
+// NOT FINISHED: This only gets the first 20 articles. In a real app,
+// you implement pagination.
 export async function fetchArticles() {
-  // In storage the ids are separated from the data, but in this function
-  // we are going to combine the id and the data together.
-  return Object.entries(articles).map(([id, data]) => ({ id, ...data }));
+  const snapshot = await getDocs(
+    query(collection(db, "articles"), orderBy("date", "desc"), limit(20))
+  )
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }))
 }
