38e9f2cdb4454f29d0d4db0f4f33d4daae7b138b
Jayden Craig <35790878+jcraig3@users.noreply.github.com>
2021-11-11 20:42:30 -0800

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,7 +5,7 @@ export default function Article({ article }) {
         <p>No article selected</p>
       ) : (
         <section>
-          <img className="popcorn" src={`${article.image}`}></img>
+          <img id="popcorn" src={`${article.image}`}></img>
           <h2>{article.title}</h2>
           <p className="author">{`By: ${article.author}`}</p>
           <p className="date">{`Posted: ${article.date}`}</p>
