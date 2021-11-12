0c318d2346b8b9f981e83afcff7edacb84380407
Jayden Craig <35790878+jcraig3@users.noreply.github.com>
2021-11-11 20:28:16 -0800

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,7 +5,7 @@ export default function Article({ article }) {
         <p>No article selected</p>
       ) : (
         <section>
-          <img src="https://yt3.ggpht.com/ytc/AKedOLSkNBClOIZNjJayMdTxRhUh6LYEXjjjCv0tMJ3-mA=s900-c-k-c0x00ffffff-no-rj">
+          <img src="https://yt3.ggpht.com/ytc/AKedOLSkNBClOIZNjJayMdTxRhUh6LYEXjjjCv0tMJ3-mA=s900-c-k-c0x00ffffff-no-rj"></img>
           <h2>{article.title}</h2>
           <p className="author">{`By: ${article.author}`}</p>
           <p className="date">{`Posted: ${article.date}`}</p>
