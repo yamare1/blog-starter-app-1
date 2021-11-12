59459067d02bf18a239bdf3991fb7ba9b0ad5ae3
Jayden Craig <35790878+jcraig3@users.noreply.github.com>
2021-11-11 20:19:09 -0800

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,6 +6,7 @@ export default function Article({ article }) {
       ) : (
         <section>
           <h2>{article.title}</h2>
+          <p className="author">{'By: ${article.author}'}</p>
           <p className="date">{`Posted: ${article.date}`}</p>
           <p className="body">{article.body}</p>
         </section>
